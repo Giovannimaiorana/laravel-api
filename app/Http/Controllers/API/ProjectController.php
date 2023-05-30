9350b01b8e36c1fd2d08ac4de3f72e08ee66fedc
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-30 17:26:28 +0200

    aggiunta get per visualizzare risultati in postman

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with(['technologies','type']);
+        $projects = Project::with(['technologies','type'])->get();
 
         return response()->json([
            'success' => true,

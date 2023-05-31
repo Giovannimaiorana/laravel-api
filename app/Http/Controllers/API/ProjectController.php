40dc09a082309757d015e8482740f762d47307f3
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-31 17:37:22 +0200

    creazione route show per front

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -9,11 +9,18 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with(['technologies','type'])->get();
+        $projects = Project::with(['technologies','type'])->paginate(6);//get();
 
         return response()->json([
            'success' => true,
            'results' => $projects
         ]);
+    }
+    public function show($id){
+        $project = Project::where('id', $id)->with(['technologies','type'])->first();
+         return response()->json([
+            'success'=>true,
+            'post'=> $project
+         ]);
     }
 }
